d4a8988e52cd56240ddb04f2744582bac649bbd5
ofats <ogfatog@gmail.com>
2019-05-28 21:58:20 +0300

    Add Google benchmark. Some build scripts enhancement.

--- build_debug.sh
+++ build_debug.sh
@@ -1,5 +1,5 @@
 cd `dirname ${BASH_SOURCE[0]}`
-mkdir -p build
-cd build
-cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+mkdir -p build/Debug
+cd build/Debug
+cmake ../.. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 make $*
