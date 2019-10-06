881ee54cda460a81c0fdaf067cd39c83e04a7314
ofats <ogfatog@gmail.com>
2019-10-06 15:23:00 +0300

    Fix testing scripts.

--- run_tests_debug.sh
+++ run_tests_debug.sh
@@ -1,4 +1,4 @@
 cd `dirname ${BASH_SOURCE[0]}`
-[ ! -d "./build" ] && echo "Error: build project first using ./build_debug.sh" && exit
+[ ! -d "./build/Debug" ] && echo "Error: build project first using ./build_debug.sh first" && exit
 cd build/Debug
 ctest $*
--- run_tests_release.sh
+++ run_tests_release.sh
@@ -1,4 +1,4 @@
 cd `dirname ${BASH_SOURCE[0]}`
-[ ! -d "./build" ] && echo "Error: build project first using ./build_release.sh" && exit
+[ ! -d "./build/Release" ] && echo "Error: build project first using ./build_release.sh first" && exit
 cd build/Release
 ctest $*
