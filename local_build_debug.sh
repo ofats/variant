25c3b44a1853b3907d2ccbea514e71964ce63d19
ofats <ogfatog@gmail.com>
2019-10-10 16:48:06 +0300

    Add -fno-sanitize-recover=undefined option to debug build.

--- local_build_debug.sh
+++ local_build_debug.sh
@@ -1,5 +1,5 @@
 cd `dirname ${BASH_SOURCE[0]}`
 mkdir -p build/Debug
 cd build/Debug
-cmake ../.. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_FLAGS="-O0 -fno-omit-frame-pointer -g -fsanitize=address,leak,undefined -Wall -Wextra -Werror"
+cmake ../.. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_FLAGS="-g -fsanitize=address,leak,undefined -fno-sanitize-recover=undefined -Wall -Wextra -Werror"
 make -j
