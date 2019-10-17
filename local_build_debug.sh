59815964562edaa62311f834cdd41532a38f81f2
ofats <ogfatog@gmail.com>
2019-10-17 15:09:13 +0300

    Rework local build scripts.

--- local_build_debug.sh
+++ local_build_debug.sh
@@ -1,5 +1,11 @@
 cd `dirname ${BASH_SOURCE[0]}`
-mkdir -p build/Debug
-cd build/Debug
-cmake ../.. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_FLAGS="-g -fsanitize=address,leak,undefined -fno-sanitize-recover=undefined -Wall -Wextra -Werror" -DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=gold -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cmake --build .
+
+BUILD_DIR=build/Debug
+CC=gcc
+CXX=g++
+CXX_FLAGS="-g -fsanitize=address,leak,undefined -fno-sanitize-recover=undefined -Wall -Wextra -Werror"
+LINKER_FLAGS="-lasan -lubsan -fuse-ld=gold"
+
+mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
+cmake ../.. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build . -j
--- local_build_release.sh
+++ local_build_release.sh
@@ -1,5 +1,11 @@
 cd `dirname ${BASH_SOURCE[0]}`
-mkdir -p build/Release
-cd build/Release
-cmake ../.. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_FLAGS="-O3 -Wall -Wextra -Werror -g" -DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=gold
-make -j
+
+BUILD_DIR=build/Release
+CC=gcc
+CXX=g++
+CXX_FLAGS="-O3 -Wall -Wextra -Werror -g"
+LINKER_FLAGS="-fuse-ld=gold"
+
+mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
+cmake ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}"
+cmake --build . -j
